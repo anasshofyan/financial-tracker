eef71ac63a2969e13f672119ce99cbd09780f52a
Anas Shofyan Martunis Fateh <shofyananasmf@gmail.com>
2024-01-04 19:50:48 +0700

    feat: Hide password field in user model

--- models/userModel.js
+++ models/userModel.js
@@ -14,6 +14,7 @@ const userSchema = new Schema(
       type: String,
       required: [true, "Password is required!"],
       minlength: 6,
+      select: false,
     },
     role: { type: String, enum: ["user", "admin"], default: "user" },
   },
