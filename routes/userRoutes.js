38295bd5a3f9db5e0eafaaf2be9d0d90ba5703f2
Anas Shofyan Martunis Fateh <shofyananasmf@gmail.com>
2024-01-04 19:17:06 +0700

    refactor: Refactor userRoutes.js to use register instead of createUser and getUsers instead of getAllUser

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const useController = require("../controllers/userController");
 
-router.post("/", useController.createUser);
-router.get("/getAllUser", useController.getUsers);
+router.post("/", useController.register);
+router.get("/", useController.getUsers);
 
 module.exports = router;
