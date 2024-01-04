14ac82b0753b1372e50b86a7e13f5f12c25b0dbc
Anas Shofyan Martunis Fateh <shofyananasmf@gmail.com>
2024-01-04 20:27:09 +0700

    feat: Add token verification middleware to user routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const useController = require("../controllers/userController");
+const verifyToken = require("../middlewares/verifyToken");
 
 router.post("/login", useController.login);
 router.post("/", useController.register);
-router.get("/", useController.getList);
-router.put("/:id", useController.update);
-router.delete("/:id", useController.deleteUser);
+router.get("/", verifyToken, useController.getList);
+router.put("/:id", verifyToken, useController.update);
+router.delete("/:id", verifyToken, useController.deleteUser);
 
 module.exports = router;
