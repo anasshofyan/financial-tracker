10bd21bb98fa143e6c9995844392dfb2641c52a2
Anas Shofyan Martunis Fateh <shofyananasmf@gmail.com>
2024-01-04 15:47:05 +0700

    refactor: Refactor userController.js to include success messages

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,9 +4,12 @@ const User = require("../models/userModel");
 exports.createUser = async (req, res) => {
   try {
     const user = await User.create(req.body);
-    res.status(200).json(user);
+    res.status(200).json({
+      success: true,
+      data: user,
+      message: "User created successfully",
+    });
   } catch (err) {
-    console.log(err.message);
     res.status(400).json({ message: err.message });
   }
 };
@@ -15,8 +18,16 @@ exports.createUser = async (req, res) => {
 exports.getUsers = async (req, res) => {
   try {
     const users = await User.find();
-    res.json(users);
+    res.json({
+      success: true,
+      data: users,
+      message: "Users retrieved successfully",
+    });
   } catch (err) {
-    res.status(500).json({ message: err.message });
+    res.status(500).json({
+      success: false,
+      message: "Failed to retrieve users",
+      error: err.message,
+    });
   }
 };
