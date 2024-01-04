d2432505d45696a5973fd65809406ffd08efbb0c
Anas Shofyan Martunis Fateh <shofyananasmf@gmail.com>
2024-01-04 19:16:35 +0700

    feat: Add user registration and get users endpoints

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,33 +1,65 @@
 const User = require("../models/userModel");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const { sendResponse } = require("../utils/response.js");
 
-// Create a new user
-exports.createUser = async (req, res) => {
+const secretKey = process.env.SECRET_KEY;
+
+const register = async (req, res) => {
   try {
-    const user = await User.create(req.body);
-    res.status(200).json({
-      success: true,
-      data: user,
-      message: "User created successfully",
+    const { username, email, password } = req.body;
+
+    const existingUser = await User.findOne({ username });
+    if (existingUser) {
+      return sendResponse(res, false, "User already exists", 400);
+    }
+
+    // Hash the password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    // Create a new user
+    const newUser = new User({
+      username,
+      email,
+      password: hashedPassword,
+    });
+
+    // Save the user to the database
+    const savedUser = await newUser.save();
+
+    // Create and return a JWT token
+    const token = jwt.sign(
+      {
+        user: {
+          id: savedUser._id,
+          username: savedUser.username,
+          email: savedUser.email,
+        },
+      },
+      secretKey
+    );
+
+    sendResponse(res, true, "User created successfully", 201, {
+      token,
+      user: savedUser,
     });
   } catch (err) {
-    res.status(400).json({ message: err.message });
+    if (err.name === "ValidationError") {
+      sendResponse(res, false, "Validation failed", 400, err.errors);
+    } else {
+      sendResponse(res, false, "Failed to create user", 500);
+    }
   }
 };
 
-// Get all users
-exports.getUsers = async (req, res) => {
+const getUsers = async (req, res) => {
   try {
     const users = await User.find();
-    res.json({
-      success: true,
-      data: users,
-      message: "Users retrieved successfully",
-    });
+    sendResponse(res, true, "Get list user success", 200, users);
   } catch (err) {
-    res.status(500).json({
-      success: false,
-      message: "Failed to retrieve users",
-      error: err.message,
-    });
+    sendResponse(res, false, "Failed to get list user", 500);
   }
 };
+
+module.exports = { register, getUsers };
