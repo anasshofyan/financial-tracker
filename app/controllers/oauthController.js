517301096be232650cce3094117e3f923d1e184b
Anas Shofyan Martunis Fateh <shofyananasmf@gmail.com>
2024-02-05 23:12:01 +0700

    feat: Update callback URL and handle errors in oauthController

--- app/controllers/oauthController.js
+++ app/controllers/oauthController.js
@@ -5,42 +5,51 @@ const { oauth2Client } = require('../config/oauthConfig.js')
 const { google } = require('googleapis')
 
 const oauthController = async (req, res) => {
-  const { code } = req.query
-  const { tokens } = await oauth2Client.getToken(code)
-  oauth2Client.setCredentials(tokens)
+  try {
+    const { code } = req.query
+    const { tokens } = await oauth2Client.getToken(code)
+    oauth2Client.setCredentials(tokens)
 
-  const oauth2 = google.oauth2({ version: 'v2', auth: oauth2Client })
+    const oauth2 = google.oauth2({ version: 'v2', auth: oauth2Client })
 
-  const { data } = await oauth2.userinfo.get()
+    const { data } = await oauth2.userinfo.get()
 
-  if (!data.email || !data.name) {
-    return sendResponse(res, false, 'Not found data', 400, {})
+    if (!data.email || !data.name) {
+      return sendResponse(res, false, 'Not found data', 400, {})
+    }
+
+    const existingUser = await User.findOne({ email: data.email })
+    if (existingUser) {
+      return sendResponse(res, false, 'User already exists', 400)
+    }
+
+    const newUser = new User({
+      googleId: data.id,
+      googleAccessToken: tokens.access_token,
+      googleRefreshToken: tokens.refresh_token,
+      username: data.name,
+      email: data.email,
+      picture: data.picture,
+    })
+
+    const savedUser = await newUser.save()
+
+    const token = generateToken(savedUser)
+
+    sendResponse(res, true, 'User Oauth Google successfully Created', 201, {
+      token,
+      user: savedUser,
+    })
+
+    try {
+      res.redirect(`${process.env.CLIENT_URL}/auth-success?token=${token}`)
+    } catch (error) {
+      console.log(error)
+    }
+  } catch (error) {
+    console.error(error)
+    sendResponse(res, false, 'Internal Server Error', 500, {})
   }
-
-  const existingUser = await User.findOne({ email: data.email })
-  if (existingUser) {
-    return sendResponse(res, false, 'User already exists', 400)
-  }
-
-  const newUser = new User({
-    googleId: data.id,
-    googleAccessToken: tokens.access_token,
-    googleRefreshToken: tokens.refresh_token,
-    username: data.name,
-    email: data.email,
-    picture: data.picture,
-  })
-
-  const savedUser = await newUser.save()
-
-  const token = generateToken(savedUser)
-
-  sendResponse(res, true, 'User Oauth Google successfully Created', 201, {
-    token,
-    user: savedUser,
-  })
-
-  res.redirect(`${process.env.CLIENT_URL}/auth-success?token=${token}`)
 }
 
 module.exports = { oauthController }
