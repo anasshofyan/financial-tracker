68e3ab8e7ebbed56181c7b734732d953bb482af4
Anas Shofyan Martunis Fateh <shofyananasmf@gmail.com>
2024-02-22 20:56:35 +0700

    feat: Remove unused import and update date format

--- app/utils/formatDate.js
+++ app/utils/formatDate.js
@@ -1,8 +1,7 @@
 const { format } = require('date-fns')
-const { id } = require('date-fns/locale')
 
 const formatDate = (date) => {
-  return format(new Date(date), 'd MMMM yyyy', { locale: id })
+  return format(new Date(date), 'd MMMM yyyy')
 }
 
 module.exports = { formatDate }
