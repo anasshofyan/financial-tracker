7120703fe483a048319320c612377c5425a13040
Anas Shofyan Martunis Fateh <shofyananasmf@gmail.com>
2024-02-22 20:52:11 +0700

    feat: Refactor date format in formatDate.js

--- app/utils/formatDate.js
+++ app/utils/formatDate.js
@@ -2,7 +2,7 @@ const { format } = require('date-fns')
 const { id } = require('date-fns/locale')
 
 const formatDate = (date) => {
-  return format(new Date(date), 'EEEE, d MMMM yyyy', { locale: id })
+  return format(new Date(date), 'd MMMM yyyy', { locale: id })
 }
 
 module.exports = { formatDate }
