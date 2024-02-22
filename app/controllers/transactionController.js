b56c86692611fefd331f9205750e9be6455add5f
Anas Shofyan Martunis Fateh <shofyananasmf@gmail.com>
2024-02-22 23:37:06 +0700

    feat: add transactionController.js and transactionRoutes.js

--- app/controllers/transactionController.js
+++ app/controllers/transactionController.js
@@ -98,12 +98,6 @@ const getList = async (req, res) => {
       transactions,
     }))
 
-    responseData.sort((a, b) => new Date(b.date) - new Date(a.date))
-
-    Object.values(groupedTransactions).forEach((transactions) => {
-      transactions.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt))
-    })
-
     sendResponse(res, true, 'Get list transaction success', 200, {
       listGroup: responseData,
       totalIncome,
@@ -112,48 +106,6 @@ const getList = async (req, res) => {
     })
   } catch (err) {
     sendResponse(res, false, 'Failed to get list transaction', 500)
-  }
-}
-
-const getMonthlySummary = async (req, res) => {
-  try {
-    const loggedInUserId = req.decoded.user.id
-    const { month, year } = req.query
-
-    const startOfMonth = new Date(year, month - 1, 1)
-    const endOfMonth = new Date(year, month, 0, 23, 59, 59, 999)
-
-    const transactions = await Transaction.find({
-      createdBy: loggedInUserId,
-      date: { $gte: startOfMonth, $lte: endOfMonth },
-    })
-
-    let monthlySummary = {}
-
-    transactions.forEach((transaction) => {
-      const formattedDate = formatDate(transaction.date, 'yyyy-MM-dd')
-      if (!monthlySummary[formattedDate]) {
-        monthlySummary[formattedDate] = {
-          totalIncome: 0,
-          totalExpense: 0,
-        }
-      }
-      if (transaction.type === 'income') {
-        monthlySummary[formattedDate].totalIncome += transaction.amount
-      } else if (transaction.type === 'expense') {
-        monthlySummary[formattedDate].totalExpense += transaction.amount
-      }
-    })
-
-    // Convert the object into an array of objects
-    const responseData = Object.entries(monthlySummary).map(([date, summary]) => ({
-      date,
-      ...summary,
-    }))
-
-    sendResponse(res, true, 'Get monthly summary success', 200, responseData)
-  } catch (err) {
-    sendResponse(res, false, 'Failed to get monthly summary', 500)
   }
 }
 
@@ -184,33 +136,13 @@ const update = async (req, res) => {
   const { id } = req.params
   const { amount, description, categoryId, date } = req.body
 
-  const loggedInUserId = req.decoded.user.id
-
   try {
-    const category = await Category.findById(categoryId)
-
-    if (!category) {
-      return sendResponse(res, false, 'Category Not Found', 400, {})
-    }
-
-    const type = category.type
-
-    const updatedTransaction = {
-      amount,
-      description,
-      category: categoryId,
-      date,
-      type,
-      createdBy: loggedInUserId,
-    }
-
-    const updated = await Transaction.findByIdAndUpdate(id, updatedTransaction, { new: true })
-
-    if (!updated) {
-      return sendResponse(res, false, 'Transaction not found', 404)
-    }
-
-    sendResponse(res, true, 'Transaction updated successfully', 200, updated)
+    const transaction = await Transaction.findByIdAndUpdate(
+      id,
+      { amount, description, categoryId, date },
+      { new: true },
+    )
+    sendResponse(res, true, 'Update transaction success', 200, transaction)
   } catch (err) {
     if (err.name === 'ValidationError') {
       sendResponse(res, false, 'Validation failed', 400, err.errors)
@@ -230,4 +162,101 @@ const deleteTransaction = async (req, res) => {
   }
 }
 
-module.exports = { create, getList, getDetail, update, deleteTransaction, getMonthlySummary }
+const formatDate1 = (date, format) => {
+  return new Intl.DateTimeFormat('en-US', { month: 'long', year: 'numeric' }).format(date)
+}
+
+// Fungsi untuk menghitung sisa saldo berdasarkan total pendapatan dan total pengeluaran
+const calculateRemainingBalance = (totalIncome, totalExpense) => {
+  return totalIncome - totalExpense
+}
+
+const getVisualizationData = async (req, res) => {
+  try {
+    const loggedInUserId = req.decoded.user.id
+    const { startDate, endDate } = req.query
+
+    const dateFilter = {
+      createdBy: loggedInUserId,
+    }
+
+    if (startDate && endDate) {
+      const start = new Date(startDate)
+      const end = new Date(endDate)
+      dateFilter.date = { $gte: start, $lte: end }
+    }
+
+    const transactions = await Transaction.find(dateFilter).populate({
+      path: 'category',
+      model: 'Category',
+    })
+
+    let groupedData = {}
+    transactions.forEach((transaction) => {
+      const transactionMonthYear = formatDate1(transaction.date, 'MMMM YYYY')
+      if (!groupedData[transactionMonthYear]) {
+        groupedData[transactionMonthYear] = {
+          totalIncome: 0,
+          totalExpense: 0,
+        }
+      }
+      if (transaction.type === 'income') {
+        groupedData[transactionMonthYear].totalIncome += transaction.amount
+      } else if (transaction.type === 'expense') {
+        groupedData[transactionMonthYear].totalExpense += transaction.amount
+      }
+    })
+
+    const chartData = Object.entries(groupedData)
+      .map(([date, data]) => {
+        const formattedDate = formatDate(new Date(date))
+        return {
+          date: formattedDate,
+          totalIncome: data.totalIncome,
+          totalExpense: data.totalExpense,
+          remainingBalance: calculateRemainingBalance(data.totalIncome, data.totalExpense),
+        }
+      })
+      .sort((a, b) => {
+        const dateA = new Date(a.date)
+        const dateB = new Date(b.date)
+        return dateB - dateA
+      })
+
+    const totalIncome = chartData.reduce((acc, curr) => acc + curr.totalIncome, 0)
+    const totalExpense = chartData.reduce((acc, curr) => acc + curr.totalExpense, 0)
+    const remainingBalance = calculateRemainingBalance(totalIncome, totalExpense)
+
+    sendResponse(res, true, 'Get visualization data success', 200, {
+      chartData,
+      totalIncome,
+      totalExpense,
+      remainingBalance,
+    })
+  } catch (err) {
+    sendResponse(res, false, 'Failed to get visualization data', 500)
+  }
+}
+function formatDate2(date) {
+  const monthNames = [
+    'Januari',
+    'Februari',
+    'Maret',
+    'April',
+    'Mei',
+    'Juni',
+    'Juli',
+    'Agustus',
+    'September',
+    'Oktober',
+    'November',
+    'Desember',
+  ]
+
+  const month = monthNames[date.getMonth()]
+  const year = date.getFullYear()
+
+  return `${month} ${year}`
+}
+
+module.exports = { create, getList, getDetail, update, deleteTransaction, getVisualizationData }
--- app/routes/transactionRoutes.js
+++ app/routes/transactionRoutes.js
@@ -7,7 +7,7 @@ router.get('/', verifyToken, transactionController.getList)
 router.get('/:id', verifyToken, transactionController.getDetail)
 router.post('/', verifyToken, transactionController.create)
 router.put('/:id', verifyToken, transactionController.update)
-router.get('/summary', verifyToken, transactionController.getMonthlySummary)
+router.post('/visualization', verifyToken, transactionController.getVisualizationData)
 router.delete('/:id', verifyToken, transactionController.deleteTransaction)
 
 module.exports = router
