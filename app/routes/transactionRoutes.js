13269e4f8680191b05e9effcbb10dc1cabe5fd38
Anas Shofyan Martunis Fateh <shofyananasmf@gmail.com>
2024-03-05 18:51:04 +0700

    feat: Add chart routes and remove visualization routes

--- app/routes/transactionRoutes.js
+++ app/routes/transactionRoutes.js
@@ -7,8 +7,6 @@ router.get('/', verifyToken, transactionController.getList)
 router.get('/:id', verifyToken, transactionController.getDetail)
 router.post('/', verifyToken, transactionController.create)
 router.put('/:id', verifyToken, transactionController.update)
-router.get('/visualization/stacked', verifyToken, transactionController.getStackedChartData)
-router.get('/visualization/pie', verifyToken, transactionController.getPieChartData)
 router.delete('/:id', verifyToken, transactionController.deleteTransaction)
 
 module.exports = router
