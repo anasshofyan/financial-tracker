f3d049117ac49c374c6ae4f179ee3f407e50bbb2
Anas Shofyan Martunis Fateh <shofyananasmf@gmail.com>
2024-03-14 23:45:54 +0700

    feat: Update walletModel.js: Change default wallet name from 'Dompet Utama' to 'Dompet Cerdas'

--- app/models/walletModel.js
+++ app/models/walletModel.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema
 const walletSchema = new Schema(
   {
     emoji: { type: String, default: '💰' },
-    name: { type: String, default: 'Dompet Utama', required: true },
+    name: { type: String, default: 'Dompet Cerdas', required: true },
     balance: { type: Number, default: 0 },
     bgWallet: { type: String, default: '#27B427' },
     createBy: { type: Schema.Types.ObjectId, ref: 'User' },
